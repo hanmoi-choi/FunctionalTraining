d78a5a84a375f3b1b88c8d6b67362bbab856ccad
Daniel Choi <daniel.choi@rea-group.com>
2018-10-08 23:16:24 +1100

    Add: Done Recursive Exercises

--- src/main/scala/com/rea/recursion/RecursionExercises.scala
+++ src/main/scala/com/rea/recursion/RecursionExercises.scala
@@ -29,35 +29,103 @@ object RecursionExercises {
   def minusOne(n: Int) = n - 1
 
   // Add two non-negative Integers together.  You are only allowed to use plusOne and minusOne above
-  def add(a: Int, b: Int): Int = ???
+  def add(a: Int, b: Int): Int = {
+    def loop(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else loop(n - 1, acc + 1)
+
+    loop(b, a)
+  }
 
   // You are not permitted to use any list functions such as map, flatMap, ++, flatten etc
-  def sum(l: List[Int]): Int = ???
+  def sum(l: List[Int]): Int = {
+    def loop(n: List[Int], acc: Int): Int = n match {
+      case Nil => acc
+      case h::tail => loop(tail, acc + h)
+    }
+
+    loop(l, 0)
+  }
 
   //Again no list functions are permitted for the following
-  def length[A](x: List[A]): Int = ???
+  def length[A](x: List[A]): Int = {
+    def loop(n: List[A], acc: Int): Int = n match {
+      case Nil => acc
+      case _ :: tail => loop(tail, acc + 1)
+    }
 
+    loop(x, 0)
+  }
   // Do you notice anything similar between sum and length? Hmm...
 
   // Mapping over a list.  You are given a List of type A and a function converting an A to a B
   // and you give back a list of type B.  No list functions allowed!
-  def map[A, B](x: List[A], f: A => B): List[B] = ???
+  def map[A, B](x: List[A], f: A => B): List[B] = {
+    def loop(n: List[A], acc: List[B]): List[B] = n match {
+      case Nil => acc
+      case h :: tail => loop(tail, acc :+ f(h))
+    }
 
+    loop(x, Nil)
+  }
   // Given a function from A => Boolean, return a list with only those item where the function returned true.
-  def filter[A](x: List[A], f: A => Boolean): List[A] = ???
+  def filter[A](x: List[A], f: A => Boolean): List[A] = {
+    def loop(n: List[A], acc: List[A]): List[A] = n match {
+      case Nil => acc
+      case h :: tail => if (f(h)) loop(tail, acc :+ h) else loop(tail, acc)
+    }
+
+    loop(x, Nil)
+  }
+
   // This pattern should be familiar by now... psst... look at add.
-  def append[A](x: List[A], y: List[A]): List[A] = ???
+  def append[A](x: List[A], y: List[A]): List[A] = {
+    def loop(n: List[A], acc: List[A]): List[A] = n match {
+      case Nil => acc
+      case h :: tail => loop(tail, acc :+ h)
+    }
+
+    loop(y, x)
+  }
 
   // Flatten a list of lists to a single list.  Remember you can't use list.flatten.  Can you use a previous
   // solution to solve this one?
-  def flatten[A](x: List[List[A]]): List[A] = ???
+  def flatten[A](x: List[List[A]]): List[A] = {
+    def loop(n: List[List[A]], acc: List[A]): List[A] = n match {
+      case Nil => acc
+      case h :: tail => loop(tail, acc:::h)
+    }
+
+    loop(x, Nil)
+  }
 
   // Follow the types.  You've done a great job getting here. Follow the types.
-  def flatMap[A, B](x: List[A], f: A => List[B]): List[B] = ???
+  def flatMap[A, B](x: List[A], f: A => List[B]): List[B] = {
+    def loop(n: List[A], acc: List[B]): List[B] = n match {
+      case Nil => acc
+      case h :: tail => loop(tail, acc:::f(h))
+    }
+
+    loop(x, Nil)
+  }
 
   // Maximum of the empty list is 0
-  def maximum(x: List[Int]): Int = ???
+  def maximum(x: List[Int]): Int = {
+    def loop(n: List[Int], acc: Int): Int = n match {
+      case Nil => acc
+      case h :: tail => if (h > acc) loop(tail, h) else loop(tail, acc)
+    }
+
+    if (x.isEmpty) 0 else loop(x, 0)
+  }
 
   // Reverse a list
-  def reverse[A](x: List[A]): List[A] = ???
+  def reverse[A](x: List[A]): List[A] = {
+    def loop(n: List[A], acc: List[A]): List[A] = n match {
+      case Nil => acc
+      case h :: tail => loop(tail, h::acc)
+    }
+
+    loop(x, Nil)
+  }
 }
